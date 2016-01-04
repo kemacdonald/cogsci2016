4a5a3ce2b7e4d43e5ecc27573ffcf6630789f650
Kyle MacDonald <kyle.macdonald@stanford.edu>
2016-01-04 13:24:59 -0500

    initial commit

---
+++ R/pdf_document.R
@@ -0,0 +1,202 @@
+#' Convert to a PDF document
+#'
+#' Format for converting from R Markdown to a PDF document.
+#'
+#' @inheritParams html_document
+#'
+#' @param fig_crop \code{TRUE} to automatically apply the \code{pdfcrop} utility
+#'   (if available) to pdf figures
+#' @param dev Graphics device to use for figure output (defaults to pdf)
+#' @param highlight Syntax highlighting style. Supported styles include
+#'   "default", "tango", "pygments", "kate", "monochrome", "espresso",
+#'   "zenburn", and "haddock". Pass \code{NULL} to prevent syntax highlighting.
+#' @param keep_tex Keep the intermediate tex file used in the conversion to PDF
+#' @param latex_engine LaTeX engine for producing PDF output. Options are
+#'   "pdflatex", "lualatex", and "xelatex".
+#' @param template Pandoc template to use for rendering. Pass "default" to use
+#'   the rmarkdown package default template; pass \code{NULL} to use pandoc's
+#'   built-in template; pass a path to use a custom template that you've
+#'   created.  See the documentation on
+#'   \href{http://johnmacfarlane.net/pandoc/demo/example9/templates.html}{pandoc
+#'   templates} for more details.
+#'
+#' @return R Markdown output format to pass to \code{\link{render}}
+#'
+#' @details
+#'
+#' See the \href{http://rmarkdown.rstudio.com/pdf_document_format.html}{online
+#' documentation} for additional details on using the \code{pdf_document} format.
+#' 
+#' Creating PDF output from R Markdown requires that LaTeX be installed.
+#'
+#' R Markdown documents can have optional metadata that is used to generate a
+#' document header that includes the title, author, and date. For more details
+#' see the documentation on R Markdown \link[=rmd_metadata]{metadata}.
+#'
+#' R Markdown documents also support citations. You can find more information on
+#' the markdown syntax for citations in the
+#' \href{http://rmarkdown.rstudio.com/authoring_bibliographies_and_citations.html}{Bibliographies
+#' and Citations} article in the online documentation.
+#' 
+#' Many aspects of the LaTeX template used to create PDF documents can be 
+#' customized using metadata. For example:
+#'
+#' \tabular{l}{
+#' \code{---} \cr
+#' \code{title: "Crop Analysis Q3 2013"} \cr
+#' \code{fontsize: 11pt} \cr
+#' \code{geometry: margin=1in} \cr
+#' \code{---}
+#' }
+#'
+#' Available metadata variables include:
+#'
+#' \describe{
+#'    \item{\code{lang}}{Document language code}
+#'    \item{\code{fontsize}}{Font size (e.g. 10pt, 11pt, 12pt)}
+#'    \item{\code{documentclass}}{LaTeX document class (e.g. article)}
+#'    \item{\code{classoption}}{Option for \code{documentclass} (e.g. oneside); may be repeated}
+#'    \item{\code{geometry}}{Options for geometry class (e.g. margin=1in); may be repeated}
+#'    \item{\code{mainfont, sansfont, monofont, mathfont}}{Document fonts (works only with xelatex and lualatex, see the \code{latex_engine} option)}
+#'    \item{\code{linkcolor, urlcolor, citecolor}}{Color for internal, external, and citation links (red, green, magenta, cyan, blue, black)}
+#'    \item{\code{linestretch}}{Options for line spacing (e.g. 1, 1.5, 3)}
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'
+#' library(rmarkdown)
+#'
+#' # simple invocation
+#' render("input.Rmd", pdf_document())
+#'
+#' # specify an option for latex engine
+#' render("input.Rmd", pdf_document(latex_engine = "lualatex"))
+#'
+#' # add a table of contents and pass an option to pandoc
+#' render("input.Rmd", pdf_document(toc = TRUE, "--listings"))
+#' }
+#'
+#' @export
+pdf_document <- function(toc = FALSE,
+                         toc_depth = 2,
+                         number_sections = FALSE,
+                         fig_width = 6.5,
+                         fig_height = 4.5,
+                         fig_crop = TRUE,
+                         fig_caption = FALSE,
+                         dev = 'pdf',
+                         highlight = "default",
+                         template = "default",
+                         keep_tex = FALSE,
+                         latex_engine = "pdflatex",
+                         includes = NULL,
+                         md_extensions = NULL,
+                         pandoc_args = NULL) {
+    
+    # base pandoc options for all PDF output
+    args <- c()
+    
+    # table of contents
+    args <- c(args, pandoc_toc_args(toc, toc_depth))
+    
+    # template path and assets
+    if (identical(template, "default")) {
+        
+        # choose the right template    
+        if (!pandoc_available("1.14"))
+            latex_template <- "default.tex"
+        else
+            latex_template <- "default-1.14.tex"
+        
+        # add to args
+        args <- c(args, "--template",
+                  pandoc_path_arg(rmarkdown_system_file(paste0("rmd/latex/", 
+                                                               latex_template))))
+        
+    } else if (!is.null(template)) {
+        args <- c(args, "--template", pandoc_path_arg(template))
+    }
+    
+    # numbered sections
+    if (number_sections)
+        args <- c(args, "--number-sections")
+    
+    # highlighting
+    if (!is.null(highlight))
+        highlight <- match.arg(highlight, highlighters())
+    args <- c(args, pandoc_highlight_args(highlight))
+    
+    # latex engine
+    latex_engine = match.arg(latex_engine, c("pdflatex", "lualatex", "xelatex"))
+    args <- c(args, pandoc_latex_engine_args(latex_engine))
+    
+    # content includes
+    args <- c(args, includes_to_pandoc_args(includes))
+    
+    # args args
+    args <- c(args, pandoc_args)
+    
+    saved_files_dir <- NULL
+    
+    pre_processor <- function(metadata, input_file, runtime, knit_meta, 
+                              files_dir, output_dir) {
+        # save files dir (for generating intermediates)
+        saved_files_dir <<- files_dir
+        
+        # use a geometry filter when we are using the "default" template
+        if (identical(template, "default"))
+            pdf_pre_processor(metadata, input_file, runtime, knit_meta, files_dir, 
+                              output_dir)
+        else
+            invisible(NULL)
+    }
+    
+    intermediates_generator <- function(original_input, encoding, 
+                                        intermediates_dir) {
+        # copy all intermediates (pandoc will need to bundle them in the PDF)
+        intermediates <- copy_render_intermediates(original_input, encoding, 
+                                                   intermediates_dir, FALSE)
+        
+        # we need figures from the supporting files dir to be available during
+        # render as well; if we have a files directory, copy its contents
+        if (!is.null(saved_files_dir) && dir_exists(saved_files_dir)) {
+            file.copy(saved_files_dir, intermediates_dir, recursive = TRUE)
+            intermediates <- c(intermediates, list.files(
+                path = file.path(intermediates_dir, basename(saved_files_dir)),
+                all.files = TRUE, recursive = TRUE, full.names = TRUE))
+        }
+        
+        intermediates 
+    }
+    
+    # return format
+    output_format(
+        knitr = knitr_options_pdf(fig_width, fig_height, fig_crop, dev),
+        pandoc = pandoc_options(to = "latex",
+                                from = from_rmarkdown(fig_caption, md_extensions),
+                                args = args,
+                                keep_tex = keep_tex),
+        clean_supporting = !keep_tex,
+        pre_processor = pre_processor,
+        intermediates_generator = intermediates_generator
+    )
+}
+
+
+# Use filter to set pdf geometry defaults (while making sure we don't override
+# any geometry settings already specified by the user)
+pdf_pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir,
+                              output_dir) {
+    
+    args <- c()
+    
+    # set the margin to 1 inch if no other geometry options specified
+    has_geometry <- function(text) {
+        length(grep("^geometry:.*$", text)) > 0
+    }
+    if (!has_geometry(readLines(input_file, warn = FALSE)))
+        args <- c(args, "--variable", "geometry:margin=1in")
+    
+    args
+}
---
+++ R/utils.R
@@ -0,0 +1,260 @@
+#' @export
+find_file <- function(template, file) {
+    template <- system.file("rmarkdown", "templates", template, file,
+                            package = "cogsci2016")
+    if (template == "") {
+        stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+    }
+    
+    template
+}
+
+#' @export
+find_resource <- function(template, file) {
+    find_file(template, file.path("resources", file))
+}
+
+createUniqueId <- function(bytes) {
+    paste(as.hexmode(sample(256, bytes)-1), collapse="")
+}
+
+is_windows <- function() {
+    identical(.Platform$OS.type, "windows")
+}
+
+# determine the output file for a pandoc conversion
+pandoc_output_file <- function(input, pandoc_options) {
+    to <- pandoc_options$to
+    if (!is.null(pandoc_options$ext))
+        ext <- pandoc_options$ext
+    else if (to %in% c("latex", "beamer"))
+        ext <- ".pdf"
+    else if (to %in% c("html", "html5", "s5", "slidy",
+                       "slideous", "dzslides", "revealjs"))
+        ext <- ".html"
+    else if (grepl("^markdown", to)) {
+        if (!identical(tolower(tools::file_ext(input)), "md"))
+            ext <- ".md"
+        else {
+            ext <- paste(".", strsplit(to, "[\\+\\-]")[[1]][[1]], sep = "")
+        }
+    }
+    else
+        ext <- paste(".", to, sep = "")
+    output <- paste(tools::file_path_sans_ext(input), ext, sep = "")
+    basename(output)
+}
+
+
+rmarkdown_system_file <- function(file) {
+    system.file(file, package = "rmarkdown")
+}
+
+from_rmarkdown <- function(implicit_figures = TRUE, extensions = NULL) {
+    
+    # paste extensions together and remove whitespace
+    extensions <- paste0(extensions, collapse = "")
+    extensions <- gsub(" ", "", extensions)
+    
+    # exclude implicit figures unless the user has added them back
+    if (!implicit_figures && !grepl("implicit_figures", extensions))
+        extensions <- paste0("-implicit_figures", extensions)
+    
+    rmarkdown_format(extensions)
+}
+
+is_null_or_string <- function(text) {
+    is.null(text) || (is.character(text) && (length(text) == 1))
+}
+
+read_lines_utf8 <- function(file, encoding) {
+    
+    # read the file
+    lines <- readLines(file, warn = FALSE)
+    
+    # normalize encoding to iconv compatible form
+    if (identical(encoding, "native.enc"))
+        encoding <- ""
+    
+    # convert to utf8
+    if (!identical(encoding, "UTF-8"))
+        iconv(lines, from = encoding, to = "UTF-8")
+    else
+        mark_utf8(lines)
+}
+
+# mark the encoding of character vectors as UTF-8
+mark_utf8 <- function(x) {
+    if (is.character(x)) {
+        Encoding(x) <- 'UTF-8'
+        return(x)
+    }
+    if (!is.list(x)) return(x)
+    attrs <- attributes(x)
+    res <- lapply(x, mark_utf8)
+    attributes(res) <- attrs
+    res
+}
+
+# TODO: remove this when fixed upstream https://github.com/viking/r-yaml/issues/6
+yaml_load_utf8 <- function(string, ...) {
+    string <- paste(string, collapse = '\n')
+    mark_utf8(yaml::yaml.load(enc2utf8(string), ...))
+}
+
+yaml_load_file_utf8 <- function(input, ...) {
+    yaml_load_utf8(readLines(input, encoding = 'UTF-8'), ...)
+}
+
+file_name_without_shell_chars <- function(file) {
+    name <- gsub(.shell_chars_regex, '_', basename(file))
+    dir <- dirname(file)
+    if (nzchar(dir) && !identical(dir, "."))
+        file.path(dir, name)
+    else
+        name
+}
+
+# return a string as a tempfile
+as_tmpfile <- function(str) {
+    if (length(str) > 0) {
+        str_tmpfile <- tempfile("rmarkdown-str", fileext = ".html")
+        writeLines(str, str_tmpfile)
+        str_tmpfile
+    } else {
+        NULL
+    }
+}
+
+dir_exists <- function(x) {
+    file_test('-d', x)
+}
+
+file_with_ext <- function(file, ext) {
+    paste(tools::file_path_sans_ext(file), ".", ext, sep = "")
+}
+
+
+file_with_meta_ext <- function(file, meta_ext, ext = tools::file_ext(file)) {
+    paste(tools::file_path_sans_ext(file),
+          ".", meta_ext, ".", ext, sep = "")
+}
+
+knitr_files_dir <- function(file) {
+    paste(tools::file_path_sans_ext(file), "_files", sep = "")
+}
+
+knitr_cache_dir <- function(file, pandoc_to) {
+    paste(tools::file_path_sans_ext(file), "_cache/", pandoc_to, "/", sep = "")
+}
+
+
+highlighters <- function() {
+    c("default",
+      "tango",
+      "pygments",
+      "kate",
+      "monochrome",
+      "espresso",
+      "zenburn",
+      "haddock")
+}
+
+merge_lists <- function (base_list, overlay_list, recursive = TRUE) {
+    if (length(base_list) == 0)
+        overlay_list
+    else if (length(overlay_list) == 0)
+        base_list
+    else {
+        merged_list <- base_list
+        for (name in names(overlay_list)) {
+            base <- base_list[[name]]
+            overlay <- overlay_list[[name]]
+            if (is.list(base) && is.list(overlay) && recursive)
+                merged_list[[name]] <- merge_lists(base, overlay)
+            else {
+                merged_list[[name]] <- NULL
+                merged_list <- append(merged_list,
+                                      overlay_list[which(names(overlay_list) %in% name)])
+            }
+        }
+        merged_list
+    }
+}
+
+strip_white <- function (x)
+{
+    if (!length(x))
+        return(x)
+    while (is_blank(x[1])) {
+        x = x[-1]
+        if (!length(x))
+            return(x)
+    }
+    while (is_blank(x[(n <- length(x))])) {
+        x = x[-n]
+        if (n < 2)
+            return(x)
+    }
+    x
+}
+
+is_blank <- function (x)
+{
+    if (length(x))
+        all(grepl("^\\s*$", x))
+    else TRUE
+}
+
+trim_trailing_ws <- function (x) {
+    sub("\\s+$", "", x)
+}
+
+
+# Find common base directory, throw error if it doesn't exist
+base_dir <- function(x) {
+    abs <- vapply(x, tools::file_path_as_absolute, character(1))
+    
+    base <- unique(dirname(abs))
+    if (length(base) > 1) {
+        stop("Input files not all in same directory, please supply explicit wd",
+             call. = FALSE)
+    }
+    
+    base
+}
+
+# Regular expression representing characters likely to be considered special by
+# the shell (require quoting/escaping)
+.shell_chars_regex <- '[ <>()|\\:&;#?*]'
+
+# Find a program within the PATH. On OSX we need to explictly call
+# /usr/bin/which with a forwarded PATH since OSX Yosemite strips
+# the PATH from the environment of child processes
+find_program <- function(program) {
+    if (Sys.info()["sysname"] == "Darwin") {
+        res <- suppressWarnings({
+            # Quote the path (so it can contain spaces, etc.) and escape any quotes 
+            # and escapes in the path itself
+            sanitized_path <- gsub("\\", "\\\\", Sys.getenv("PATH"), fixed = TRUE)      
+            sanitized_path <- gsub("\"", "\\\"", sanitized_path, fixed = TRUE)
+            system(paste("PATH=\"", sanitized_path, "\" /usr/bin/which ", program, sep=""),
+                   intern = TRUE)
+        })
+        if (length(res) == 0)
+            ""
+        else
+            res
+    } else {
+        Sys.which(program)
+    }
+}
+
+# given a string, escape the regex metacharacters it contains:
+# regex metas are these,
+#   . \ | ( ) [ { ^ $ * + ?
+# as defined here:
+#   http://stat.ethz.ch/R-manual/R-devel/library/base/html/regex.html
+escape_regex_metas <- function(in_str) {
+    gsub("([.\\|()[{^$+?])", "\\\\\\1", in_str)
+}
